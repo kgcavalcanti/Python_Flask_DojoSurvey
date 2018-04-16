55b7bf1c2d00055c0c86fea78e8cb8df6ed50fb7
kgcavalcanti <kgcavalcanti@outlook.com>
2018-04-16 16:16:59 -0700

    Adding validation to survey fields

--- server.py
+++ server.py
@@ -1,18 +1,23 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session, flash
 app = Flask(__name__)
+app.secret_key = 'fauiwefj09a3458390458'
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
 
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    name = request.form['name']
-    location = request.form['location']
-    language = request.form['language']
-    comments = request.form['comments']
-    print request.form
-    return render_template('summary.html', name = name, location = location, language = language, comments = comments)
+	session['name'] = request.form['name']
+	session['location'] = request.form['location']
+	session['language'] = request.form['language']
+	session['comments'] = request.form['comments']
+	if len(session['name']) < 1 or len(session['comments']) < 1:
+		flash("Name or comments cannot be empty!")
+		return redirect('/')
+	else:
+		return render_template('summary.html')
 
  
 app.run(debug=True)
